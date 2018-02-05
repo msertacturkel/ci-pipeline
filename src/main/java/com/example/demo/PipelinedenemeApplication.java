2a75e35e4ac5ccca14a8cdf3714be944558e17fc
Mustafa Sertac Turkel <sertac.turkel@etstur.com>
2018-02-05 10:33:53 +0300

    Sample Project added.

---
+++ src/main/java/com/example/demo/PipelinedenemeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelinedenemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelinedenemeApplication.class, args);
+		System.out.println("... Pipeline deneme ...");
+	}
+}
